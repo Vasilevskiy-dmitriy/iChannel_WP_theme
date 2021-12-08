9b286fea2f6a74b7754cce44c642b2c9c60b9580
Dmitriy <dima.vasilenko.com@gmail.com>
2021-12-08 15:48:56 +0200

    issue#2: fixSearch

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 
-<form role="search" method="get" id="searchform" action="" >
+<form role="search" method="get" id="searchform" action="/" >
     <input type="text" value="<?php echo get_search_query() ?>" name="s" id="s" placeholder="поиск" />
 </form>
